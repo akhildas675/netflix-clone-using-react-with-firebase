523b282f036cfe4728de4cf793fc41116462ffc2
AKHILDAS H <146437180+akhildas675@users.noreply.github.com>
2025-03-22 14:37:55 +0530

    Completed movie player and TMDB integration successfully

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import { Routes,Route } from "react-router-dom"
 import Home from "./Pages/Home/Home"
 import Login from "./Pages/Login/Login"
+import Player from "./Pages/Player/Player"
 
 
 
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/login" element={<Login/>}/>
+        <Route path="/player/:id" element={<Player/>}/>
       </Routes>
     </div>
   )
