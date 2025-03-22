1eabd0f3f44971836a6d7f541932e5d59f104775
AKHILDAS H <146437180+akhildas675@users.noreply.github.com>
2025-03-22 18:39:31 +0530

    Setup Firebase integration with .env
    Added proper Toaster alerts for errors and success
    
    Implemented loading icons for better UX
    
    Improved responsiveness across devices

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
-import React from "react"
-import { Routes,Route } from "react-router-dom"
+import React, { useEffect } from "react"
+import { Routes,Route, useNavigate } from "react-router-dom"
 import Home from "./Pages/Home/Home"
 import Login from "./Pages/Login/Login"
 import Player from "./Pages/Player/Player"
+import { onAuthStateChanged } from "firebase/auth"
+import { auth } from "../Firebase/Firebase"
+import { ToastContainer, } from 'react-toastify';
 
-
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
+  const navigate=useNavigate()
+
+  useEffect(()=>{
+    onAuthStateChanged(auth,async (user)=>{
+      if(user){
+        console.log("Logged In");
+        navigate('/')
+        
+      }else{
+        console.log("Logged Out");
+        navigate('/login')
+        
+      }
+    })
+  },[])
 
   return (
     <div>
+      <ToastContainer theme="dark"/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/login" element={<Login/>}/>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,20 +6,28 @@ import profile_img from '../../assets/profile_img.png'
 import caret_icon from '../../assets/caret_icon.svg'
 
 import './Navbar.css'
+import { logout } from '../../../Firebase/Firebase';
 
 const Navbar = () => {
 
     const navRef=useRef();
-    useEffect(()=>{
-        window.addEventListener('scroll',()=>{
-            if(window.scrollY>=80){
-                navRef.current.classList.add('nav-dark')
-            }else{
-                navRef.current.classList.remove('nav-dark')
-
+    useEffect(() => {
+        const handleScroll = () => {
+          if (navRef.current) {
+            if (window.scrollY >= 80) {
+              navRef.current.classList.add("nav-dark");
+            } else {
+              navRef.current.classList.remove("nav-dark");
             }
-        })
-    },[])
+          }
+        };
+    
+        window.addEventListener("scroll", handleScroll);
+    
+        return () => {
+          window.removeEventListener("scroll", handleScroll);
+        };
+      }, []);
 
     return (
         <div ref={navRef} className='navbar'>
@@ -43,7 +51,7 @@ const Navbar = () => {
                 <img src={profile_img} alt="" className='profile' />
                 <img src={caret_icon} alt=""  />
                 <div className="dropdown">
-                    <p>Sign out of Netflix</p>
+                    <p onClick={()=>{logout()}}>Sign out of Netflix</p>
                 </div>
 
                 </div>
